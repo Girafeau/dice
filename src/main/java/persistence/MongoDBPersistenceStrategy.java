1cbab2dab96540c8e5fb3eeb90859f2651a265c2
LeoANDRE <leo12.andre@gmail.com>
2021-03-08 00:08:19 +0100

    UP method persist(int score)

--- src/main/java/persistence/MongoDBPersistenceStrategy.java
+++ src/main/java/persistence/MongoDBPersistenceStrategy.java
@@ -1,39 +1,40 @@
 package persistence;
 
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
-import entities.HighScore;
-
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+
+import entities.HighScore;
+
 public class MongoDBPersistenceStrategy implements PersistenceStrategy {
 
-    private DBCollection collection;
+	private DBCollection collection;
 
-    public MongoDBPersistenceStrategy(String url) {
-        try {
-            MongoClient client = new MongoClient(url);
-            this.collection = client.getDB("HighScore").getCollection("scores");
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-    }
+	public MongoDBPersistenceStrategy(String url) {
+		try {
+			MongoClient client = new MongoClient(url);
+			this.collection = client.getDB("HighScore").getCollection("scores");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void persist(int score) {
+	public void persist(int score) {
+		this.collection.insert(BasicDBObjectBuilder.start().add("score", score).get());
+	}
 
-
-    }
-
-    public void load() {
-        DBCursor cursor = this.collection.find();
-        List<Integer> scores = new ArrayList<>();
-        while (cursor.hasNext()) {
-            scores.add((Integer) cursor.one().get("score"));
-            cursor.next();
-        }
-        HighScore.getInstance().setScores(scores);
-    }
+	public void load() {
+		DBCursor cursor = this.collection.find();
+		List<Integer> scores = new ArrayList<>();
+		while (cursor.hasNext()) {
+			scores.add((Integer) cursor.one().get("score"));
+			cursor.next();
+		}
+		HighScore.getInstance().setScores(scores);
+	}
 }
--- src/main/java/persistence/SQLPersistenceStrategy.java
+++ src/main/java/persistence/SQLPersistenceStrategy.java
@@ -1,41 +1,56 @@
 package persistence;
 
-import entities.HighScore;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import entities.HighScore;
+
 public class SQLPersistenceStrategy implements PersistenceStrategy {
 
-    private Connection connection;
+	private Connection connection;
 
-    public SQLPersistenceStrategy(String url, String login, String password) {
-        try {
-            this.connection = DriverManager.getConnection(url, login, password);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
+	public SQLPersistenceStrategy(String url, String login, String password) {
+		try {
+			this.connection = DriverManager.getConnection(url, login, password);
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
+	}
 
-    @Override
-    public void persist(int score) {
+	@Override
+	public void persist(int score) {
+		String request = "INSERT INTO HighScore(score) VALUES (?)";
+		try {
+			PreparedStatement ps = this.connection.prepareStatement(request);
 
-    }
+			int i = 1;
+			ps.setInt(i++, score);
 
-    @Override
-    public void load() {
-        String request = "SELECT * FROM HighScore";
-        try {
-            Statement statement = this.connection.createStatement();
-            ResultSet set = statement.executeQuery(request);
-            List<Integer> scores = new ArrayList<>();
-            while (set.next()) {
-                scores.add(set.getInt("score"));
-            }
-            HighScore.getInstance().setScores(scores);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void load() {
+		String request = "SELECT * FROM HighScore";
+		try {
+			Statement statement = this.connection.createStatement();
+			ResultSet set = statement.executeQuery(request);
+			List<Integer> scores = new ArrayList<>();
+			while (set.next()) {
+				scores.add(set.getInt("score"));
+			}
+			HighScore.getInstance().setScores(scores);
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
+	}
 }
